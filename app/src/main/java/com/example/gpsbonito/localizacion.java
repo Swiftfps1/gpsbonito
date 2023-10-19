d0c6818b988e2a7303e5aedf1700a290d37a1452
vitoc <vitocomix28@gmail.com>
2023-10-19 16:20:52 -0300

    v1

---
+++ app/src/main/java/com/example/gpsbonito/localizacion.java
@@ -0,0 +1,14 @@
+package com.example.gpsbonito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class localizacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_localizacion);
+    }
+}
